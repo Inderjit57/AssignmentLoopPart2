8e2aec4679b4e055aaececd92e0be99b0feb8982
Inderjit57 <singhinderjit57@yahoo.com>
2022-01-13 05:49:15 -0500

    Variable name and counter values are changed

--- src/UsingForLoop.java
+++ src/UsingForLoop.java
@@ -1,14 +1,26 @@
+
+/*
+ * Q1) Write a program which takes username and password from user via console
+ * and compares it with a user name and password saved in your class. Saved user
+ * id and password are as below 
+ * UserId pivotAdmin 
+ * password Admin123 
+ * If the username and password entered by user matches with the one saved in your
+ * class, then print “You are logged in to the application“ other wise print
+ * “Incorrect User id or password.Try again”. Also a user gets 3 chances enter
+ * the correct user name and password. If the user enters incorrect user name or
+ * password more than 3 times then print “ Account locked”
+ */
 import java.util.Scanner;
 
 public class UsingForLoop {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int atempts = 0;
+		int attempts = 1; // Counter for outside the ForLoop
 
-		// Number of attempts
-		for (int counter = 0; counter <= 3; counter++)
+		// Number of attempts. Here Counter is also number of attempts
+		for (int counter = 1; counter <= 3; counter++)
 		// program should go in the FOR loop and then ask for userID and Password
 
 		{
@@ -25,11 +37,11 @@ public class UsingForLoop {
 					break;
 				}
 
-				// Comparing UserID and password with boolean, if they are incorrect
+				// Comparing .equals method with boolean, if UserID and Password are incorrect
 			} else if (userID.equals("pivotAdmin") == false && password.equals("Admin123") == false) {
 				System.out.println("Incorrect User id or password.Try again");
-				atempts++;
-				if (atempts == 4) {
+				attempts++;
+				if (attempts == 4) {
 					System.out.println("Account locked");
 				}
 			}
--- src/UsingWhileLoop.java
+++ src/UsingWhileLoop.java
@@ -1,32 +1,33 @@
+
+/*
+ * Q1) Write a program which takes username and password from user via console
+ * and compares it with a user name and password saved in your class. Saved user
+ * id and password are as below 
+ * UserId pivotAdmin 
+ * password Admin123 
+ * If the username and password entered by user matches with the one saved in your
+ * class, then print “You are logged in to the application“ other wise print
+ * “Incorrect User id or password.Try again”. Also a user gets 3 chances enter
+ * the correct user name and password. If the user enters incorrect user name or
+ * password more than 3 times then print “ Account locked”
+ */
 import java.util.Scanner;
 
 public class UsingWhileLoop {
 
 	public static void main(String[] args) {
-		/*
-		 * Q1) Write a program which takes username and password from user via console
-		 * and compares it with a user name and password saved in your class. Saved user
-		 * id and password are as below 
-		 * UserId pivotAdmin 
-		 * password Admin123 
-		 * If the username and password entered by user matches with the one saved in your
-		 * class, then print “You are logged in to the application“ other wise print
-		 * “Incorrect User id or password.Try again”. Also a user gets 3 chances enter
-		 * the correct user name and password. If the user enters incorrect user name or
-		 * password more than 3 times then print “ Account locked”
-		 */
 
 		Scanner sc = new Scanner(System.in);
 
-		int counter = 0; // Number of attempts
+		int attempt = 0; // Number of attempts
 
 		// program should go in the while loop and then ask for userID and Password
-		while (counter <= 2) {
-			counter++; // Counter value change when loop starts again
+		while (attempt <= 3) {
+			attempt++; // Counter value change when loop starts again
 
-			System.out.println(counter + " Enter UserID");
+			System.out.println(attempt + " Enter UserID"); // Valid User ID - pivotAdmin
 			String userID = sc.next(); // UserID variable from console
-			System.out.println(counter + " Enter Password");
+			System.out.println(attempt + " Enter Password"); // valid password - Admin123
 			String password = sc.next(); // Password variable from console
 
 			// Using Strg.equals(Strng1) method to compare strings (If userID and Password
@@ -40,7 +41,7 @@ public class UsingWhileLoop {
 				System.out.println("Incorrect User id or password.Try again");
 			}
 		}
-		if (counter == 3) {
+		if (attempt == 4) {
 			System.out.println("Account locked");
 		}
 
